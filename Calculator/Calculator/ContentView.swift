f2fb7ba5b89a3566fadfc12b3c0b586b6bf73b45
LY <coderyliu@gmail.com>
2020-12-05 15:05:32 +0800

    feat: 计算器界面的搭建

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -8,19 +8,84 @@
 
 import SwiftUI
 
+let scale: CGFloat = UIScreen.main.bounds.width / 414
+
 struct ContentView: View {
+    
     var body: some View {
-        Text("+")
-            .font(.system(size: 38))
-            .foregroundColor(.white)
-            .frame(width: 88, height: 88)
-            .background(Color("operatorBackground"))
-            .cornerRadius(44)
+        VStack(alignment: .center, spacing: 12, content: {
+            Spacer()
+            Text("0")
+                .font(.system(size: 76))
+                .minimumScaleFactor(0.5)
+                .padding(.trailing, 24)
+                .lineLimit(1)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+            CalculatorButtonPad()
+                .padding(.bottom)
+        })
+        .scaleEffect(scale)
+    }
+}
+
+struct CalculatorButtonPad: View {
+    let pad: [[CalculatorButtonItem]] = [
+        [.command(.clear), .command(.flip), .command(.percent), .op(.divide)],
+        [.digit(7), .digit(8), .digit(9), .op(.multiply)],
+        [.digit(4), .digit(5), .digit(6), .op(.minus)],
+        [.digit(1), .digit(2), .digit(3), .op(.plus)],
+        [.digit(0), .dot, .op(.equal)]
+    ]
+    
+    var body: some View {
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 8, content: {
+            ForEach(pad, id: \.self) { row in
+                CalculatorButtonRow(row: row)
+            }
+        })
+    }
+}
+
+struct CalculatorButtonRow: View {
+    let row: [CalculatorButtonItem]
+    var body: some View {
+        HStack {
+            ForEach(row, id: \.self) { item in
+                CalculatorButton(
+                    title: item.title, size: item.size, backgroundColorName: item.backgroundColorName) {
+                    print("Button:\(item.title)")
+                }
+            }
+        }
+    }
+}
+
+struct CalculatorButton: View {
+    let fontSize: CGFloat = 38
+    let title: String
+    let size: CGSize
+    let backgroundColorName: String
+    let action:() -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.system(size: fontSize))
+                .foregroundColor(.white)
+                .frame(width: size.width, height: size.height)
+                .background(Color(backgroundColorName))
+                .cornerRadius(size.width/2)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+//                .environment(\.colorScheme, .dark)
+//            ContentView().previewDevice("iPhone SE (2nd generation)")
+//            ContentView().previewDevice("iPad Air (3rd generation)")
+        }
     }
 }
